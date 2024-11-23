3e9cce00e0f66ac17728d82cf15adb0c94afb6f2
Logan Bennett <bennett.j.logan@gmail.com>
2024-11-23 14:28:59 -0800

    add script to get papers by DOI

---
+++ paper.py
@@ -0,0 +1,36 @@
+import argparse
+from metapub import PubMedFetcher
+from datetime import datetime
+
+
+def main():
+    """
+    Fetch paper metadata from DOI and write to papers.yml
+    """
+    # Parser
+    parser = argparse.ArgumentParser(description="Fetch paper metadata from DOI")
+    parser.add_argument("--doi", type=str, help="DOI of the paper")
+    parser.add_argument(
+        "--read-on", type=str, help="Date read in YYYY-MM-DD format (defaults to today)"
+    )
+    args = parser.parse_args()
+
+    # Fetching paper metadata
+    fetch = PubMedFetcher()
+    article = fetch.article_by_doi(args.doi)
+
+    # Get date - from flag or defaults to today
+    read_date = args.read_on if args.read_on else datetime.now().strftime("%Y-%m-%d")
+
+    # Writing to papers.yml
+    with open("papers.yml", "a") as file:
+        file.write("  - title: " + repr(article.title) + "\n")
+        file.write("    authors: " + repr(article.authors) + "\n")
+        file.write("    journal: " + repr(article.journal) + "\n")
+        file.write("    year: " + str(article.year) + "\n")
+        file.write("    doi: " + repr(args.doi) + "\n")
+        file.write("    read_on: '" + read_date + "'\n")
+
+
+if __name__ == "__main__":
+    main()
